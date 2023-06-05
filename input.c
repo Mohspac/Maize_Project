6531a9254119eeb0ff4651de3c118acb81a15abf
olawale fatokun <ola.wale9@gmail.com>
2023-06-05 16:23:21 +0000

    Good fix

---
+++ input.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ncurses.h>
+
+void handle_input() {
+    int key = getch();
+
+    if (key == KEY_UP) {
+        move_forward();
+    } else if (key == KEY_DOWN) {
+        move_backward();
+    } else if (key == KEY_LEFT) {
+        strafe_left();
+    } else if (key == KEY_RIGHT) {
+        strafe_right();
+    }
+}
+
+void handle_key_press() {
+    while (1) {
+        handle_input();
+    }
+}
